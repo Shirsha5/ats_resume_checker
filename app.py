82ea846ee03ec92b34c13ee021ee0966fc423ae2
Shirsha5 <shirshamukherjee01@gmail.com>
2025-10-06 11:54:38 +0530

    updated

--- app.py
+++ app.py
@@ -288,5 +288,5 @@ def internal_error(error):
     return render_template('error.html', error_code=500, error_message='Internal server error'), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))
-    app.run(debug=False, host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
